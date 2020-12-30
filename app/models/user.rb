a6e0c36b28fe8ce89d1428e2268043b048773fec
Shin H <nobunegi13@gmail.com>
2020-12-30 20:42:21 +0900

    user_modelにaccount_name_columnを追加しDBに登録できるよう設定した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
-#  account_name           :string           not null
+#  account_name           :string           default(""), not null
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  remember_created_at    :datetime
